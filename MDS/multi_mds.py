bf66dd3a5e9edddf4de004c9ab20bb2775aa4cbf
ksk911211 <sk42@princeton.edu>
2025-02-11 18:30:30 -0800

    bugfix for load

--- MDS/multi_mds.py
+++ MDS/multi_mds.py
@@ -1,15 +1,14 @@
 from multiprocessing import Process, Queue, Manager, Value
 from MDS import mds 
-from getmds import get 
-import numpy as np
-import sys 
+import numpy as np 
+import sys
 
 class multi_mds():
 	
 	def __init__(self,shot,device,CH,t0,t1,sampling):
 		self._shot = shot
 		self._device = device
-		self._ch = CH  # No index! It is CH number (CH can't be zero!) 
+		self._ch = CH 
 		self._t0 = t0
 		self._t1 = t1 
 		self._sampling = sampling
@@ -25,8 +24,6 @@ class multi_mds():
 			num = num.zfill(2)
 			if self._device == 'MCT':
 				name = 'MC1T' + num
-			elif self._device == 'MCP':
-				name = 'MC1P' + num
 			elif self._device == 'ECE':
 				name = 'ECE' + num
 			else: pass; 
@@ -34,13 +31,13 @@ class multi_mds():
 
 	def mds_load(self,node,output):
 		g = mds('kstar',self._shot)
-		res_name = node
-		tt, yy = get(g,res_name,self._t0,self._t1,self._sampling) # for 50kHz analysis 
+		res_name = 'resample(\\' + node + ',' +str(self._t0) + ',' + str(self._t1) + ',' + str(self._sampling) + ')'
+		tt, yy = g.get(res_name)
 		ch_num = int(node[-2:]) 
 		output.put([ch_num,tt,yy]);
 		with self.lock:
 			self.count.value += 1;
-		msg = 'Parallel MDS loading for %6s > %i%%'%(node,100*self.count.value/self.node_num) 
+		msg = 'Parallel MDS loading for %6s > %i%%'%(node,100*self.count.value/self.node_num)
 		sys.stdout.write('\r>>> %s '%(msg))
 		sys.stdout.flush()
 
